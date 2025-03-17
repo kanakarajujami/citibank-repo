d0baf7e2ab2af193ddfe7ac36e406d3f1bb846ea
kanakarajujami <kanakarajujami409@gmail.com>
2025-03-17 16:10:47 +0530

    Initial project added team lead

---
+++ corejava_proj_01/src/corejava_proj_01/Arithmatic.java
@@ -0,0 +1,8 @@
+package corejava_proj_01;
+
+public class Arithmatic {
+
+	  public void add(int a,int b) {
+		  System.out.println("sum of two numbers:"+(a+b));
+	  }
+}
---
+++ corejava_proj_01/src/corejava_proj_01/MainClass.java
@@ -0,0 +1,11 @@
+package corejava_proj_01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Arithmatic addition=new Arithmatic();
+		addition.add(10, 20);
+	}
+
+}
