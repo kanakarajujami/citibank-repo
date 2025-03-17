d0031909c1b6aa3337e790ef4394598484b2f470
kanakarajujami <kanakarajujami409@gmail.com>
2025-03-17 21:11:15 +0530

    developer added interest functionality

--- corejava_proj_01/src/com/nt/insurence/IssueingLoan.java
+++ corejava_proj_01/src/com/nt/insurence/IssueingLoan.java
@@ -5,4 +5,8 @@ public class IssueingLoan {
 	public void loan() {
 		System.out.println("Loan approved:"+100000);
 	}
+	
+	public void interest() {
+		System.out.println("Interest amount is 5 percentage:");
+	}
 }
